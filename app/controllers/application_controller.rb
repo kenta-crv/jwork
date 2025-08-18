53d7e390ac83d3fd7e19d2fef752377f0648e716
kenta-crv <okuyamakenta@gmail.com>
2025-08-18 17:03:24 +0900

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
 private
- def after_sing_in_path_for(resource)
+ def after_sign_in_path_for(resource)
    case resource
    when Admin
     clients_path
