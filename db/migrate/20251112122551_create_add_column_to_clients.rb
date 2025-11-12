86858b27c1481d8336506ecea34fc482c8135ce0
kenta-crv <okuyamakenta@gmail.com>
2025-11-12 22:28:55 +0900

    fix

--- db/migrate/20251112122551_create_add_column_to_clients.rb
+++ db/migrate/20251112122551_create_add_column_to_clients.rb
@@ -1,8 +1,5 @@
 class CreateAddColumnToClients < ActiveRecord::Migration[5.2]
   def change
-    create_table :add_column_to_clients do |t|
-      add_column :clients, :rental, :string
-      t.timestamps
-    end
+    add_column :clients, :rental, :string
   end
 end
