e36f42fcc76ab64366e8368eae99384ca18acdfa
kenta-crv <okuyamakenta@gmail.com>
2025-11-12 21:16:04 +0900

    fix

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -50,16 +50,16 @@ end
   
     if @client.update(client_params)
       # conclusion.html.slimからの送信で、かつ同意が得られた場合
-      if @client.agree == "同意しました"
+      #if @client.agree == "同意しました"
           # メール送信処理
-          ClientMailer.contract_received_email(@client).deliver_now
-          ClientMailer.contract_send_email(@client).deliver_now
-          flash[:notice] = "契約が完了しました"
-          redirect_to client_path(@client)
+      #    ClientMailer.contract_received_email(@client).deliver_now
+      #    ClientMailer.contract_send_email(@client).deliver_now
+      #    flash[:notice] = "契約が完了しました"
+      #    redirect_to client_path(@client)
         # edit.html.slimからの送信、またはconclusion.html.slimからの送信でも同意が得られなかった場合
-      else
-        redirect_to client_path(@client)
-      end
+      #else
+      #  redirect_to client_path(@client)
+      #end
     else
       # 更新が失敗した場合の処理
       render :edit
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,4 @@
 class Client < ApplicationRecord
-    has_many :jobs
-    has_many :situations
+  has_many :jobs, dependent: :destroy
+  has_many :situations, dependent: :destroy
 end
