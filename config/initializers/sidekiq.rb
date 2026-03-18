6a1faef7be1daf71c16cb92a06d502d8f869dc07
kenta-crv <okuyamakenta@gmail.com>
2026-03-18 22:49:31 +0900

    user add nil

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,8 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  # namespace を 'jwork' に指定して、tcareproと場所を分ける
+  config.redis = { url: 'redis://localhost:6379/0', namespace: 'jwork' }
 end
 
+Sidekiq.configure_client do |config|
+  config.redis = { url: 'redis://localhost:6379/0', namespace: 'jwork' }
+end
