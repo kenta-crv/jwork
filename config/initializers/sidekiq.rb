9e8156acdc4cf016da61a722d215600d9543b99d
kenta-crv <okuyamakenta@gmail.com>
2026-03-18 22:01:18 +0900

    user add nil

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,8 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDIS_URL'] }
-end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  # Sidekiq 起動時に全ジョブクラスをロードする
+  Rails.application.config.after_initialize do
+    Rails.application.eager_load!  # これで app/jobs 以下の全クラスをロード
+  end
 end
