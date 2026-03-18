c99888fd5586938f957dd84f63597ae2ab5fc0f5
kenta-crv <okuyamakenta@gmail.com>
2026-03-18 22:03:29 +0900

    user add nil

--- app/jobs/call_user_job.rb
+++ app/jobs/call_user_job.rb
@@ -2,49 +2,24 @@ class CallUserJob < ApplicationJob
   queue_as :default
 
   def perform(user_id)
-    # 1. 時間帯判定ロジック (一時的にコメントアウト)
-    # now = Time.current
-    # hour = now.hour
-    #
-    # if hour >= 20 || hour < 11
-    #   scheduled_time = hour >= 20 ? now.tomorrow.change(hour: 11, min: 0) : now.change(hour: 11, min: 0)
-    #   Rails.logger.info "夜間禁止時間帯のため、User ID: #{user_id} への発信を #{scheduled_time} に延期します。"
-    #   CallUserJob.set(wait_until: scheduled_time).perform_later(user_id)
-    #   return
-    # end
-
     user = User.find_by(id: user_id)
-    return if user.nil? || user.tel.blank?
+    return unless user
 
-    # 2. 電話番号のクリーニングと日本番号の判定
-    raw_tel = user.tel.sub(/^p:/, '').gsub(/[^\d+]/, '')
+    client = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
 
-    formatted_to = nil
-    if raw_tel.start_with?('+81')
-      formatted_to = raw_tel
-    elsif raw_tel.start_with?('0')
-      formatted_to = "+81#{raw_tel[1..-1]}"
-    end
+    app_host = (Rails.env.development? && !ENV['APP_HOST']) ? 'nondisastrous-sheri-arabinosic.ngrok-free.dev' : 'j-work.jp'
+    ivr_url = "https://#{app_host}/users/#{user.id}/show_ivr"
 
-    if formatted_to.nil?
-      Rails.logger.warn "User ID: #{user.id} の番号 (#{user.tel}) は日本国内の番号ではないためスキップします。"
-      return
-    end
-
-    # 3. 発信処理
-    begin
-      client = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
-      
-      ivr_url = "https://j-work.jp/users/#{user.id}/show_ivr"
-
-      client.calls.create(
-        from: ENV['TWILIO_PHONE_NUMBER'],
-        to: formatted_to,
-        url: ivr_url
-      )
-      Rails.logger.info "User ID: #{user.id} へのIVR発信に成功しました。宛先: #{formatted_to}"
-    rescue => e
-      Rails.logger.error "IVR発信エラー (User ID: #{user_id}): #{e.message}"
-    end
+    to_number = user.tel.sub(/^p:/, '')
+    client.calls.create(
+      from: ENV['TWILIO_PHONE_NUMBER'],
+      to: to_number,
+      url: ivr_url
+    )
+    
+    Rails.logger.info "IVR call sent to: #{user.tel} URL: #{ivr_url}"
+  rescue => e
+    Rails.logger.error "IVR Job Error: #{e.message}"
+    raise e
   end
 end
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,4 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDIS_URL'] }
-
-  # Sidekiq 起動時に全ジョブクラスをロードする
-  Rails.application.config.after_initialize do
-    Rails.application.eager_load!  # これで app/jobs 以下の全クラスをロード
-  end
 end
+
