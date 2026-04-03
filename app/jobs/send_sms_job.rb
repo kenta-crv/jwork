d01ef286cb46b5ee6afdb1d4ab72468738ca00b6
kenta-crv <okuyamakenta@gmail.com>
2026-04-03 22:45:11 +0900

    fix

--- app/jobs/send_sms_job.rb
+++ app/jobs/send_sms_job.rb
@@ -1,21 +1,28 @@
+# app/jobs/send_sms_job.rb
 class SendSmsJob < ApplicationJob
   queue_as :default
 
   def perform(user_id)
     user = User.find_by(id: user_id)
     return if user.nil? || user.tel.blank?
-    
-    # 先頭に "p:" がある場合だけ削除
-    raw_tel = user.tel.start_with?("p:") ? user.tel[2..-1] : user.tel
-    # 数字以外（ハイフンなど）を除去
-    raw_tel = raw_tel.gsub(/[^\d]/, '')
 
-    # ★重要：090... を +8190... に変換するロジック（IVRの成功パターンを移植）
-    if raw_tel.match?(/^0\d{9,10}$/)
-      to_number = "+81#{raw_tel[1..-1]}"
-    else
-      to_number = raw_tel # すでに+81がついている場合など
-    end
+    # 先頭に "p:" がある場合だけ削除
+    raw_tel = user.tel.to_s.strip.sub(/^p:/, '')
+
+    # 数字と + 以外を除去
+    raw_tel = raw_tel.gsub(/[^\d+]/, '')
+
+    # 個別送信と同じルールで +81 に変換
+    to_number =
+      if raw_tel.match?(/^0\d{9,11}$/)       # 例: 08012345678
+        "+81#{raw_tel[1..-1]}"
+      elsif raw_tel.match?(/^81\d{9,11}$/)   # 例: 818012345678
+        "+#{raw_tel}"
+      elsif raw_tel.start_with?('+')          # すでに +81 形式
+        raw_tel
+      else
+        raw_tel
+      end
 
     client = Twilio::REST::Client.new(
       ENV['TWILIO_ACCOUNT_SID'],
