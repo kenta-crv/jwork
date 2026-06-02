f5d210c3031411f963f354ecb5653dacdff2b1de
kenta-crv <okuyamakenta@gmail.com>
2026-06-02 15:27:03 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -22,10 +22,6 @@ Rails.application.routes.draw do
 
   get "top/policy" => 'top#policy'  
 
-  get 'cargo', to: 'pages#cargo'
-  get 'human',  to: 'pages#human'
-  get 'logstic',  to: 'pages#logstic'
-  get 'event',       to: 'pages#event'
 
   #get "/database" => 'top#database' # ユーザー側トップ
   get '/redirect', to: 'top#redirect'
